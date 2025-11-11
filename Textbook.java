f9c3a137b598fc2bbe053fcb8351a8afb24334f5
suhanimohanty <suhanimohanty1110@gmail.com>
2025-11-10 22:34:16 -0500

    Added Textbook.java

---
+++ Textbook.java
@@ -0,0 +1,28 @@
+public class Textbook extends Book {
+    private int edition;
+
+    // Constructor
+    public Textbook(String title, double price, int edition) {
+        super(title, price);
+        this.edition = edition;
+    } 
+    // Getter method for subject
+    public int getEdition() {
+        return this.edition;
+    }
+
+
+
+    public static void main(String[] args) {
+        Textbook bio2015 = new Textbook("Biology", 49.75, 2);
+        Textbook bio2019 = new Textbook("Biology", 39.75, 3);
+        System.out.println("Textbook 1 Edition: " + bio2015.getEdition());
+        System.out.println("Textbook 1 Title: " + bio2015.getTitle());
+        System.out.println("Textbook 1 Price: " + bio2015.getPrice());
+        System.out.println("Textbook 2 Edition: " + bio2019.getEdition());
+        System.out.println("Textbook 2 Title: " + bio2019.getTitle());
+        System.out.println("Textbook 2 Price: " + bio2019.getPrice());
+        
+    }
+}
+    
